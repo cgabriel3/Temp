0eed620761b2f08302e34b8e1d61b84ed90a712e
cgabriel3 <christian.gabriel.isjwara@fintopia.tech>
2023-11-23 17:06:17 +0800

    Revert "Revert "Fix try except returns""
    This reverts commit 5960b1f29cd064231ba2572dafa71e745e18cf0b.

--- sync_tasks/tapd.py
+++ sync_tasks/tapd.py
@@ -14,25 +14,23 @@ class Tapd:
 
   def get_stories(self):
     get_stories_api = f'api/tapd/external/story/getStoryBySource?source={self.project}'
-    story_list = []
 
     try:
       story_list = self.send_tapd_request_get(get_stories_api)
+      return story_list
     except Exception as e:
       logging.error(f'Failed to get stories from Tapd. Error: {e}')
-
-    return story_list
+      return []
 
   def get_comments(self):
     get_comments_api = f'api/tapd/external/comment/getCommentBySource?source={self.project}'
-    comment_list = []
 
     try:
       comment_list = self.send_tapd_request_get(get_comments_api)
+      return comment_list
     except Exception as e:
       logging.error(f'Failed to get comments from Tapd. Error: {e}')
-
-    return comment_list
+      return []
 
   def get_stories_new(self):
     get_stories_api = '/api/tapd/external/common/getEntryBySource/story/'
@@ -50,29 +48,26 @@ class Tapd:
 
   def get_task(self):
     get_tasks_api = '/api/tapd/external/common/getEntryBySource/task/'
-    task_list = []
     request_body = {
       "workspace_id": self.workspace_id
     }
 
     try:
       task_list = self.send_tapd_request_post(get_tasks_api, request_body=request_body)
+      return task_list
     except Exception as e:
       logging.error(f'Failed to get stories from Tapd. Error: {e}')
-
-    return task_list
+      return []
 
   def get_images(self, image_path):
     get_image_api = f'api/tapd/external/image/{self.project}?workspaceId={self.workspace_id}&imagePath={image_path}'
-    image = {}
 
     try:
       response = self.send_tapd_request_get(get_image_api)
-      image = response['data']['Attachment']['download_url']
+      return response['data']['Attachment']['download_url']
     except Exception as e:
       logging.error(f'Failed to get comments from Tapd. Error: {e}')
-
-    return image
+      return {}
 
   def send_tapd_request_get(self, method):
     for i in range(self.max_retries):
@@ -80,7 +75,7 @@ class Tapd:
         response = requests.get(self.api_url + method, timeout=30)
         response.raise_for_status()
         return response.json()
-  
+
       except requests.exceptions.RequestException as e:
         if i < self.max_retries - 1:
           print("Retrying...")
@@ -96,7 +91,7 @@ class Tapd:
         response = requests.post(self.api_url + method + self.project, json=request_body, timeout=30)
         response.raise_for_status()
         return response.json()
-  
+
       except requests.exceptions.RequestException as e:
         if i < self.max_retries - 1:
           print("Retrying...")
